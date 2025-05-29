987f1f5dc644c3e3d88b74e4aae6f34065e6bfaa
Adnan <abdifo@gmail.com>
2025-05-28 20:35:09 -0400

    fixing some more theme issues

--- meditation-app-v2/src/App.tsx
+++ meditation-app-v2/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { Layout } from './components/Layout'
 
 function App() {
   return (
-    <div className="app">
-      <h1>Meditation App</h1>
-    </div>
+    <Layout>
+      <div className="space-y-8">
+        <h1 className="text-display-2 font-display text-primary-600 dark:text-primary-400">
+          Meditation App
+        </h1>
+        <p className="text-body-lg text-earth-700 dark:text-earth-300">
+          Begin your journey to mindfulness
+        </p>
+      </div>
+    </Layout>
   )
 }
 
