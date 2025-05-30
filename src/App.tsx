d0f53a6b1d9f2effece507d2311d10c1d7ce787d
Adnan <abdifo@gmail.com>
2025-05-30 19:39:08 -0400

    react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useState } from 'react';
 import { Header } from './components/Header';
 import Timer from './components/Timer';
 import { SessionHistory } from './components/SessionHistory';
